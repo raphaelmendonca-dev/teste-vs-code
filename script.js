a80e2cc900779c7eb97f409001123e6ef982e833
Raphael <raphaelmendonca@hotmail.com>
2025-12-07 06:18:17 -0300

    Conectando JS e Python

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-function mostrarMensagem() {
-alert("Uma vez Flamengo, Flamengo até Morrer 🚀");
+// Função que busca dados na API Python
+async function buscarDadosDoPython() {
+    try {
+        // O JavaScript vai lá na url /api/tempo buscar os dados
+        const resposta = await fetch('/api/tempo');
+        
+        // Ele converte o texto estranho (JSON) para um objeto legível
+        const dados = await resposta.json();
+        
+        // Agora pegamos o elemento da tela e trocamos o texto
+        const elemento = document.getElementById('mensagem-python');
+        elemento.innerText = `🐍 ${dados.saudacao} Hora certa: ${dados.hora_servidor}`;
+        
+    } catch (erro) {
+        console.error("Erro ao buscar dados:", erro);
+    }
 }
 
-// O comando abaixo busca o botão e fica "ouvindo" o clique
+// Quando clicar no botão, chama a função acima
 document.querySelector('.botao').addEventListener('click', function(event) {
-    // Evita que o link abra uma nova aba (comportamento padrão) só para testarmos o JS
-    event.preventDefault(); 
-    mostrarMensagem();
+    event.preventDefault(); // Não deixa mudar de página
+    buscarDadosDoPython();  // Chama o Python
 });
