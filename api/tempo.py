f326b5b9321b8561fe8095554b7d34dbaad42ced
Raphael <raphaelmendonca@hotmail.com>
2025-12-07 18:06:28 -0300

    Migrando para Flask

--- api/tempo.py
+++ api/tempo.py
@@ -1,24 +1,28 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 import json
+import requests  # Importando a biblioteca que pedimos no requirements.txt
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # Pega a hora atual
-        agora = datetime.now().strftime("%d/%m/%Y às %H:%M:%S")
+        # 1. Vamos buscar o Dólar na API externa
+        url = "https://economia.awesomeapi.com.br/last/USD-BRL"
+        resposta_externa = requests.get(url)
+        dados_dolar = resposta_externa.json()
         
-        # Cria uma resposta simples
+        # O valor vem dentro de ['USDBRL']['bid']
+        valor_dolar = dados_dolar['USDBRL']['bid']
+        # Arredondando para ficar bonito (ex: 5.42)
+        valor_formatado = f"R$ {float(valor_dolar):.2f}"
+        
+        # 2. Montar nossa resposta
         mensagem = {
-            "saudacao": "Olá do Python!",
-            "hora_servidor": agora,
-            "linguagem": "Python 3"
+            "saudacao": f"Dólar Agora: {valor_formatado}",
+            "hora_servidor": datetime.now().strftime("%H:%M:%S")
         }
         
-        # Prepara para enviar de volta ao navegador
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        
-        # Envia a mensagem convertida para texto (JSON)
         self.wfile.write(json.dumps(mensagem).encode('utf-8'))
         return
